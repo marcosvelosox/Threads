ba36744b1514cf6f988657cc7a37eb697074bf21
marcosvelosox <70241255+marcosvelosox@users.noreply.github.com>
2024-09-10 20:38:09 -0300

    Create Cavaleiro.java

---
+++ Cavaleiro.java
@@ -0,0 +1,40 @@
+package threads;
+
+import java.util.Random;
+
+public class Cavaleiro {
+    String nome;
+    int posicao = 0;
+    int velocidade;
+    boolean temTocha = false;
+    boolean temPedra = false;
+
+    public Cavaleiro(String nome) {
+        this.nome = nome;
+        // Velocidade inicial entre 2 e 4 metros por 50 ms
+        this.velocidade = new Random().nextInt(3) + 2;
+    }
+
+    public void andar() {
+        this.posicao += this.velocidade;
+    }
+
+    public void pegarTocha() {
+        if (!temTocha) {
+            temTocha = true;
+            velocidade += 2;
+        }
+    }
+
+    public void pegarPedra() {
+        if (!temPedra && !temTocha) {
+            temPedra = true;
+            velocidade += 2;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return nome + " está na posição " + posicao + "m com velocidade de " + velocidade + "m/50ms";
+    }
+}
